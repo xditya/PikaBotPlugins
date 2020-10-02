2bd65737ac3768a7124ebdbc75a4f3a34c88cc4b
Sachin Jangir <38481181+ItzSjDude@users.noreply.github.com>
2020-10-02 05:49:46 +0530

    Update blacklistf.py

--- plugins/blacklistf.py
+++ plugins/blacklistf.py
@@ -3,9 +3,9 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """Filters
 Available Commands:
-.addblacklist
-.listblacklist
-.rmblacklist"""
+{i}addblacklist <Blacklist Word>
+{i}listblacklist |list all Blacklist words|
+{i}rmblacklist <Removal word> """
 import asyncio
 import io
 import re
@@ -51,90 +51,86 @@ async def on_new_message(event):
                 sqlx.rf_blx(event.chat_id, snip.lower())
             break
 
-@borg.on(admin_cmd(pattern="addblacklist ((.|\n)*)"))
+@ItzSjDude(pattern="addblacklist$ ((.|\n)*)")
 async def on_add_black_list(event):
-    text = event.pattern_match.group(1)
-    to_blacklist = list(set(trigger.strip() for trigger in text.split("\n") if trigger.strip()))
-    for trigger in to_blacklist:
-        sql.add_to_blacklist(event.chat_id, trigger.lower())
-    await event.edit("Added {} triggers to the blacklist in the current chat".format(len(to_blacklist)))
+    if event.from_id == bot.uid:
+      text = event.pattern_match.group(1)
+      to_blacklist = list(set(trigger.strip() for trigger in text.split("\n") if trigger.strip()))
+      for trigger in to_blacklist:
+          sql.add_to_blacklist(event.chat_id, trigger.lower())
+      await event.edit("Added {} triggers to the blacklist in the current chat".format(len(to_blacklist)))
+    if event.from_id==bot2.uid:
+      text = event.pattern_match.group(1)
+      to_blacklist = list(set(trigger.strip() for trigger in text.split("\n") if trigger.strip()))
+      for trigger in to_blacklist:
+          sqlx.a_to_blx(event.chat_id, trigger.lower())
+      await event.edit("Added {} triggers to the blacklist in the current chat".format(len(to_blacklist)))
 
 
-@borg.on(admin_cmd(pattern="listblacklist"))
+@ItzSjDude(pattern="listblacklist$")
 async def on_view_blacklist(event):
-    all_blacklisted = sql.get_chat_blacklist(event.chat_id)
-    OUT_STR = "Blacklists in the Current Chat:\n"
-    if len(all_blacklisted) > 0:
-        for trigger in all_blacklisted:
-            OUT_STR += f"👉 {trigger} \n"
-    else:
-        OUT_STR = "No BlackLists. Start Saving using `.addblacklist`"
-    if len(OUT_STR) > Config.MAX_MESSAGE_SIZE_LIMIT:
-        with io.BytesIO(str.encode(OUT_STR)) as out_file:
-            out_file.name = "blacklist.text"
-            await event.client.send_file(
-                event.chat_id,
-                out_file,
-                force_document=True,
-                allow_cache=False,
-                caption="BlackLists in the Current Chat",
-                reply_to=event
-            )
-            await event.delete()
-    else:
-        await event.edit(OUT_STR)
+    if event.from_id==bot.uid:
+      all_blacklisted = sql.get_chat_blacklist(event.chat_id)
+      OUT_STR = "Blacklists in the Current Chat:\n"
+      if len(all_blacklisted) > 0:
+          for trigger in all_blacklisted:
+              OUT_STR += f"👉 {trigger} \n"
+      else:
+          OUT_STR = "No BlackLists. Start Saving using `.addblacklist`"
+      if len(OUT_STR) > Config.MAX_MESSAGE_SIZE_LIMIT:
+          with io.BytesIO(str.encode(OUT_STR)) as out_file:
+              out_file.name = "blacklist.text"
+              await bot.send_file(
+                  event.chat_id,
+                  out_file,
+                  force_document=True,
+                  allow_cache=False,
+                  caption="BlackLists in the Current Chat",
+                  reply_to=event
+              )
+              await event.delete()
+      else:
+          await event.edit(OUT_STR)
+    if bot2 is not None and event.from_id==bot2.uid
+      all_blacklisted = sqlx.gc_blx(event.chat_id)
+      OUT_STR = "Blacklists in the Current Chat:\n"
+      if len(all_blacklisted) > 0:
+          for trigger in all_blacklisted:
+              OUT_STR += f"👉 {trigger} \n"
+      else:
+          OUT_STR = "No BlackLists. Start Saving using `.addblacklist`"
+      if len(OUT_STR) > Config.MAX_MESSAGE_SIZE_LIMIT:
+          with io.BytesIO(str.encode(OUT_STR)) as out_file:
+              out_file.name = "blacklist.text"
+              await bot2.send_file(
+                  event.chat_id,
+                  out_file,
+                  force_document=True,
+                  allow_cache=False,
+                  caption="BlackLists in the Current Chat",
+                  reply_to=event
+              )
+              await event.delete()
+      else:
+          await event.edit(OUT_STR)
 
 
-@borg.on(admin_cmd(pattern="rmblacklist ((.|\n)*)"))
+
+@ItzSjDude(pattern="rmblacklist$ ((.|\n)*)")
 async def on_delete_blacklist(event):
-    text = event.pattern_match.group(1)
-    to_unblacklist = list(set(trigger.strip() for trigger in text.split("\n") if trigger.strip()))
-    successful = 0
-    for trigger in to_unblacklist:
-        if sql.rm_from_blacklist(event.chat_id, trigger.lower()):
-            successful += 1
-    await event.edit(f"Removed {successful} / {len(to_unblacklist)} from the blacklist")
-
-@bot2.on(admin_cmd(pattern="addblacklist ((.|\n)*)"))
-async def on_add_black_list(event):
-    text = event.pattern_match.group(1)
-    to_blacklist = list(set(trigger.strip() for trigger in text.split("\n") if trigger.strip()))
-    for trigger in to_blacklist:
-        sqlx.a_to_blx(event.chat_id, trigger.lower())
-    await event.edit("Added {} triggers to the blacklist in the current chat".format(len(to_blacklist)))
-
-
-@bot2.on(admin_cmd(pattern="listblacklist"))
-async def on_view_blacklist(event):
-    all_blacklisted = sqlx.gc_blx(event.chat_id)
-    OUT_STR = "Blacklists in the Current Chat:\n"
-    if len(all_blacklisted) > 0:
-        for trigger in all_blacklisted:
-            OUT_STR += f"👉 {trigger} \n"
-    else:
-        OUT_STR = "No BlackLists. Start Saving using `.addblacklist`"
-    if len(OUT_STR) > Config.MAX_MESSAGE_SIZE_LIMIT:
-        with io.BytesIO(str.encode(OUT_STR)) as out_file:
-            out_file.name = "blacklist.text"
-            await event.client.send_file(
-                event.chat_id,
-                out_file,
-                force_document=True,
-                allow_cache=False,
-                caption="BlackLists in the Current Chat",
-                reply_to=event
-            )
-            await event.delete()
-    else:
-        await event.edit(OUT_STR)
-
-
-@bot2.on(admin_cmd(pattern="rmblacklist ((.|\n)*)"))
-async def on_delete_blacklist(event):
-    text = event.pattern_match.group(1)
-    to_unblacklist = list(set(trigger.strip() for trigger in text.split("\n") if trigger.strip()))
-    successful = 0
-    for trigger in to_unblacklist:
-        if sqlx.rf_blx(event.chat_id, trigger.lower()):
-            successful += 1
-    await event.edit(f"Removed {successful} / {len(to_unblacklist)} from the blacklist")
+    if event.chat_id == bot.uid:
+      text = event.pattern_match.group(1)
+      to_unblacklist = list(set(trigger.strip() for trigger in text.split("\n") if trigger.strip()))
+      successful = 0
+      for trigger in to_unblacklist:
+          if sql.rm_from_blacklist(event.chat_id, trigger.lower()):
+              successful += 1
+      await event.edit(f"Removed {successful} / {len(to_unblacklist)} from the blacklist")
+    if bot2 is not None and event.from_id==bot.uid:
+      text = event.pattern_match.group(1)
+      to_unblacklist = list(set(trigger.strip() for trigger in text.split("\n") if trigger.strip()))
+      successful = 0
+      for trigger in to_unblacklist:
+          if sqlx.rf_blx(event.chat_id, trigger.lower()):
+              successful += 1
+      await event.edit(f"Removed {successful} / {len(to_unblacklist)} from the blacklist")
