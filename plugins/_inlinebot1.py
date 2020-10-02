8912c17805b36e09860830ba4551471efbdad6ac
Sachin Jangir <38481181+ItzSjDude@users.noreply.github.com>
2020-10-02 06:01:45 +0530

    Update _inlinebot1.py

--- plugins/_inlinebot1.py
+++ plugins/_inlinebot1.py
@@ -87,7 +87,8 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
     ))
     async def on_plug_in_callback_query_handler(event):
         plugin_name = event.data_match.group(1).decode("UTF-8")
-        help_string = CMD_LIST[plugin_name].__doc__
+        help_string = CMD_LIST[plugin_name].__doc__.format(x=rx)
+
         reply_pop_up_alert = help_string if help_string is not None else \
             "No DOCSTRING has been setup for {} plugin".format(plugin_name)
         reply_pop_up_alert += "\n\n Use .unload {} to remove this plugin\n\
