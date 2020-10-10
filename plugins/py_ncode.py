d4cf803d99929b2793e4e349a78d0b81efe67b00
Sachin Jangir <support@ItzSjDude.in>
2020-10-10 13:58:05 +0530

    Update and rename ncode (2).py to py_ncode.py

---
+++ plugins/py_ncode.py
@@ -0,0 +1,28 @@
+"""Beautifies nd convert python code into image
+{i}ncode <reply to file for normal image>
+{i}ncode doc <reply to file for document format> 
+""""
+#created by @Buddhhu, Rebased by @ItzSjDude. All Rights Reserved 
+
+import pygments, os, asyncio
+from pygments.lexers import Python3Lexer
+from pygments.formatters import ImageFormatter
+
+@ItzSjDude(pattern=r"ncode (.*)")
+async def coder_print(event):
+        input=event.pattern_match.group(1) 
+	a = await event.client.download_media(await event.get_reply_message(), Var.TEMP_DOWNLOAD_DIRECTORY)
+	s = open(a, 'r')
+	c = s.read()
+	s.close()
+	pygments.highlight(f"{c}", Python3Lexer(), ImageFormatter(font_name="DejaVu Sans Mono", line_numbers=True), "out.png")
+        if input=='doc'
+	  await event.client.send_file(event.chat_id, "out.png", force_document=True)
+          await event.delete()
+	  os.remove(a)
+	  os.remove('out.png')
+	else:
+            await event.client.send_file(event.chat_id, "out.png", force_document=False)
+	    await event.delete()
+	    os.remove(a)
+	    os.remove('out.png')
