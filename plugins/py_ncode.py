72b4d07303af087399fd672c2ba8245e214bc097
Sachin Jangir <support@ItzSjDude.in>
2020-10-10 14:12:57 +0530

    Update py_ncode.py

--- plugins/py_ncode.py
+++ plugins/py_ncode.py
@@ -11,9 +11,9 @@ from pygments.formatters import ImageFormatter
 @ItzSjDude(pattern=r"ncode (.*)")
 async def coder_print(event):
         input=event.pattern_match.group(1) 
-	a = await event.client.download_media(await event.get_reply_message(), Var.TEMP_DOWNLOAD_DIRECTORY)
-	s = open(a, 'r')
-	c = s.read()
+	a=await event.client.download_media(await event.get_reply_message(), Var.TEMP_DOWNLOAD_DIRECTORY)
+	s=open(a, 'r')
+	c=s.read()
 	s.close()
 	pygments.highlight(f"{c}", Python3Lexer(), ImageFormatter(font_name="DejaVu Sans Mono", line_numbers=True), "out.png")
         if input=='doc'
