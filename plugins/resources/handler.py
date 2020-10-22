8c48328f6b70d2b663e756f491344ec7ccfe41c2
Sachin Jangir <support@ItzSjDude.in>
2020-10-22 14:23:35 +0530

    Update handler.py

--- plugins/resources/handler.py
+++ plugins/resources/handler.py
@@ -2,20 +2,20 @@ import os
 try:
   from pikabot import bot,bot2,bot3,bot4
   i1=bot.uid; i2=bot2.uid; i3=bot3.uid; i4=bot4.uid
-  from_id=sender_id 
+   
 except:
     pass
 
 def pikaa(a, shortname):
     try:
       AS = os.environ.get(f"{shortname}", None).split('|')
-      if a.from_id == i1:
+      if a.sender_id == i1:
         return AS[0]
-      if a.from_id == i2:
+      if a.sender_id == i2:
         return AS[1]
-      if a.from_id == i3:
+      if a.sender_id == i3:
         return AS[2]
-      if a.from_id == i4:
+      if a.sender_id == i4:
         return AS[3]
       else:
          pass
