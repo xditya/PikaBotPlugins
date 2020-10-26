957a1f2a5af2acaf4d1db2331cb43fe70bf6f532
Sachin Jangir <support@ItzSjDude.in>
2020-10-26 21:21:37 +0530

    Update reverse.py

--- plugins/reverse.py
+++ plugins/reverse.py
@@ -1,8 +1,11 @@
+
+
 # Copyright (C) 2019 The Raphielscape Company LLC.
 # Thanks to @kandnub, for this awesome module !!
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
-""" Userbot module for reverse searching stickers and images on Google """
+"""Reverse searching stickers and images on Google
+{i}imgsearch <reply to image>"""
 
 import io
 import os
