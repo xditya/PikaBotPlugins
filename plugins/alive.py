81465f6fffbfe5efe3f678b3ffbd42442ecebb7d
Sachin Jangir <support@ItzSjDude.in>
2020-10-27 17:07:01 +0530

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -5,14 +5,14 @@
 
 import asyncio
 from telethon import events
+from pikabot import *
 from pikabot.main_plugs.plug import *
-from pikabot.utils import ItzSjDude
-from telethon import events
-
+from pikabot.utils import *
+from pikabot.utils import get_readable_time as grt
 
 @ItzSjDude(outgoing=True,pattern=r"alive$")
 async def _(event):
-     
+     pupt=grt((time.time() - UpTime))
      pix=pikaa(event, "ALIVE_PIC")
      if pix is not None:
         pic=pix
@@ -20,6 +20,6 @@ async def _(event):
         pic=apic
      az=pikaa(event, "ALIVE_NAME")
      await event.delete() 
-     a=await event.client.send_file(event.chat_id, pic,caption=alivestr.format(az))
+     a=await event.client.send_file(event.chat_id, pic,caption=alivestr.format(az,pupt))
      await asyncio.sleep(15)
      await a.delete()
