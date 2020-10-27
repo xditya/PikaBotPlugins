8aa31691ae7509a166e11e5dc92d86db33dcb3ef
Sachin Jangir <support@ItzSjDude.in>
2020-10-27 17:25:08 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -17,3 +17,5 @@ async def _(event):
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     await event.edit("✪  𝗣𝗂𝗄𝖺 𝗣𝗂𝗄𝖺 𝗣𝗂𝗄𝖺𝖼𝗁𝗎 !\n➥ {} Ms\n➥ 𝑴𝒚 𝑩𝒐𝒔𝒔 **{}**".format(ms,az))
+    await asyncio.sleep(7)
+    await event.delete()
